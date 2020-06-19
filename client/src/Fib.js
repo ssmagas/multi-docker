859c86e5d0a1a439bc71f97c2d3e1f8e1b5bad58
ssmagas <kakavose@gmail.com>
2020-06-19 09:48:26 +0300

    changed Fib.js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -14,12 +14,12 @@ class Fib extends Component {
   }
 
   async fetchValues() {
-    const values = await axios.get("api/values/current");
+    const values = await axios.get("/api/values/current");
     this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get("api/values/all");
+    const seenIndexes = await axios.get("/api/values/all");
     this.setState({ seenIndexes: seenIndexes.data });
   }
 
